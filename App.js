db9cfd705003b29ec91aab3850a23fa11edab51c
Kryštof Woldřich <krystof51@gmail.com>
2018-09-16 23:25:23 +0300

    [No changes] Trying to connect local git user with gitlab.com (3rd try)

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
