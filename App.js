cd32293d1b99b7bb0482c1fecdf02a26f620dca1
Kryštof Woldřich <krystof51@gmail.com>
2018-09-19 09:52:41 +0300

    [Repair] Wrong path to the redux library.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import LoginScreen from "./components/LoginScreen";
-import { createStore } from "../../Users/kwoldrich/Library/Caches/typescript/3.0/node_modules/redux";
+import { createStore } from "redux";
 import reducers from "./reducers";
 
 export default () => (
