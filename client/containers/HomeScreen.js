7ea99374d3dd976d4cc9aeb17528ac1dadbee7d7
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-09-24 19:48:29 +0300

    Add tip box on how to use phraze

--- client/containers/HomeScreen.js
+++ client/containers/HomeScreen.js
@@ -11,7 +11,7 @@ const newPhrazes = [
     key: '1',
     public: true,
     phrazed: false,
-    favorite: true,
+    favorite: false
   },
   {
     phraze: 'Ban ten gi la gi va song o dau?',
@@ -19,15 +19,15 @@ const newPhrazes = [
     key: '2',
     public: true,
     phrazed: false,
-    favorite: true,
+    favorite: false
   },
   {
     phraze: 'Ban ten gi la gi va song o dau?',
     translated: "What's your name and where do you live?",
     key: '3',
-    public: true,
+    public: false,
     phrazed: false,
-    favorite: true,
+    favorite: true
   },
   {
     phraze: 'Ban ten gi la gi va song o dau?',
@@ -35,7 +35,7 @@ const newPhrazes = [
     key: '4',
     public: true,
     phrazed: false,
-    favorite: true,
+    favorite: true
   },
   {
     phraze: 'Ban ten gi la gi va song o dau?',
@@ -43,14 +43,14 @@ const newPhrazes = [
     key: '5',
     public: true,
     phrazed: false,
-    favorite: true,
-  },
+    favorite: false
+  }
 ];
 
 class HomeScreen extends Component {
   static navigationOptions = {
     drawerLabel: 'Home',
-    drawerIcon: <Icon name="home" color="#4AA9A8" />,
+    drawerIcon: <Icon name="home" color="#4AA9A8" />
   };
 
   state = {
@@ -62,26 +62,26 @@ class HomeScreen extends Component {
         key: '6',
         public: true,
         phrazed: false,
-        favorite: true,
+        favorite: true
       },
       {
         phraze: 'Ban thich MERN Stack hay khong?',
         translated: 'Do you have any experiences in MERN Stack?',
         key: '7',
-        public: true,
-        phrazed: false,
-        favorite: true,
+        public: false,
+        phrazed: true,
+        favorite: true
       },
       {
         phraze: 'Toi dang xem anime Attack on Titan',
         translated: 'I am watching Attack on Titan anime.',
         key: '8',
-        public: true,
+        public: false,
         phrazed: true,
-        favorite: true,
-      },
+        favorite: false
+      }
     ],
-    page: 1,
+    page: 1
   };
 
   componentDidMount() {
@@ -95,17 +95,16 @@ class HomeScreen extends Component {
       if (this.state.page === 1) {
         this.setState({
           phrazes: this.state.phrazes.concat(newPhrazes),
-          page: 2,
+          page: 2
         });
       }
       this.setState({
-        refreshing: false,
+        refreshing: false
       });
     }, 1000);
   };
 
   onPressItem = (key, opt) => {
-    console.log(key, opt)
     const newPhrazes = this.state.phrazes.map(el => {
       if (el.key === key) {
         switch (opt) {
@@ -136,7 +135,6 @@ class HomeScreen extends Component {
             checkedIcon="people"
             uncheckedIcon="people"
             checkedColor="#33AAAA"
-            size={30}
             checked={item.public}
             onPress={() => this.onPressItem(item.key, 'public')}
           />
@@ -146,14 +144,13 @@ class HomeScreen extends Component {
             checkedIcon="check-box"
             uncheckedIcon="check-box-outline-blank"
             checkedColor="#33AAAA"
-            size={30}
             checked={item.phrazed}
             onPress={() => this.onPressItem(item.key, 'phrazed')}
           />
         </View>
         <View style={styles.col2}>
-          <Text style={{ fontSize: 20 }}>{item.phraze}</Text>
-          <Text style={{ color: '#B2B2B2', fontSize: 20 }}>
+          <Text style={{ fontSize: 16 }}>{item.phraze}</Text>
+          <Text style={{ color: '#B2B2B2', fontSize: 16 }}>
             {item.translated}
           </Text>
         </View>
@@ -169,7 +166,6 @@ class HomeScreen extends Component {
             uncheckedIcon="star-border"
             checkedColor="#F6BF26"
             checked={item.favorite}
-            size={30}
             onPress={() => this.onPressItem(item.key, 'favorite')}
           />
         </View>
@@ -182,6 +178,24 @@ class HomeScreen extends Component {
 
     return (
       <View style={styles.container}>
+        <View style={styles.phrazeTip}>
+          <View style={styles.col1}>
+            <Icon name="check-box" color="white" />
+          </View>
+          <View
+            style={{
+              flex: 0.7,
+              paddingHorizontal: 10
+            }}
+          >
+            <Text style={{ fontSize: 16 }}>
+              Tap a select box if you have learned the phraze.
+            </Text>
+          </View>
+          <View style={styles.col4}>
+            <Icon name="cancel" color="#848484" />
+          </View>
+        </View>
         <FlatList
           refreshing={this.state.refreshing}
           onRefresh={this.fetchData}
@@ -198,39 +212,46 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  phrazeTip: {
+    paddingVertical: 10,
+    backgroundColor: '#D7D7D7',
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   phrazeContainer: {
     paddingVertical: 23,
     flexDirection: 'row',
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderBottomColor: '#ccc'
   },
   col1: {
     flex: 0.2,
     paddingHorizontal: 10,
+    alignItems: 'center'
   },
   col2: {
     flex: 0.6,
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
   },
   col3: {
     flex: 0.1,
-    paddingHorizontal: 10,
+    paddingHorizontal: 10
   },
   col4: {
     flex: 0.1,
     paddingHorizontal: 10,
     justifyContent: 'space-between',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   checkBoxContainer: {
     backgroundColor: '#FFFFFF',
     borderWidth: 0,
     padding: 0,
     margin: 0,
-    width: 30,
-  },
+    width: 24
+  }
 });
 
 export default HomeScreen;
