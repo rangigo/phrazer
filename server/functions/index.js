2d888720a7f0a8ec3fea1a5face61b3689846af0
Kryštof Woldřich <krystof51@gmail.com>
2018-09-24 15:49:53 +0300

    [Add] test endpoint to try the automatic deploy

--- server/functions/index.js
+++ server/functions/index.js
@@ -7,4 +7,8 @@ app.get("/home", (request, response) => {
   response.send("Hello from firebase express app!");
 });
 
+app.get("/user", (request, response) => {
+  response.send("Ha ha, you smart as*.");
+});
+
 exports.app = functions.https.onRequest(app);
