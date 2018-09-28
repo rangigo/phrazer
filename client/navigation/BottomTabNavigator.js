b2b591e9f50455b33db9375e36167257176d4566
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-09-28 15:38:57 +0300

    Restructure logic of basic stack wrapper

--- client/navigation/BottomTabNavigator.js
+++ client/navigation/BottomTabNavigator.js
@@ -1,58 +1,55 @@
-import React from "react";
+import React from 'react';
 
-import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
-import StackWrapper from "./BasicStackNavigatorWrapper";
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import StackWrapper from './BasicStackNavigatorWrapper';
 
-import { Icon } from "react-native-elements";
+import { Icon } from 'react-native-elements';
 
-import HomeScreen from "./../containers/HomeScreen";
-import ChatScreen from "./../containers/ChatScreen";
-import FeedScreen from "./../containers/FeedScreen";
-
-const menuIcon = <Icon name="menu" color="white" />;
-const searchIcon = <Icon name="search" color="white" />;
+import HomeScreen from './../containers/HomeScreen';
+import ChatScreen from './../containers/ChatScreen';
+import FeedScreen from './../containers/FeedScreen';
 
 const TabNavigator = createMaterialBottomTabNavigator(
   {
     HomeStack: {
-      screen: StackWrapper(menuIcon, searchIcon, "Home", { HomeScreen }),
+      screen: StackWrapper('menu', 'search', 'Home', { HomeScreen }),
       navigationOptions: {
-        tabBarLabel: "Home",
-        tabBarIcon: ({ tintColor }) => <Icon name="home" color={tintColor} />
-      }
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ tintColor }) => <Icon name="home" color={tintColor} />,
+      },
     },
     FeedStack: {
-      screen: StackWrapper(menuIcon, searchIcon, "Feed", { FeedScreen }),
+      screen: StackWrapper('menu', 'search', 'Feed', { FeedScreen }),
       navigationOptions: {
-        tabBarLabel: "Feed",
-        tabBarIcon: ({ tintColor }) => <Icon name="public" color={tintColor} />
-      }
+        tabBarLabel: 'Feed',
+        tabBarIcon: ({ tintColor }) => <Icon name="public" color={tintColor} />,
+      },
     },
     ChatStack: {
-      screen: StackWrapper(menuIcon, searchIcon, "Chat", { ChatScreen }),
+      screen: StackWrapper('menu', 'search', 'Chat', { ChatScreen }),
       navigationOptions: {
-        tabBarLabel: "Chat",
+        tabBarLabel: 'Chat',
         tabBarIcon: ({ tintColor }) => (
           <Icon name="question-answer" color={tintColor} />
-        )
-      }
+        ),
+      },
     },
     ProfileStack: {
-      screen: StackWrapper(menuIcon, null, "Profile", { ChatScreen }),
+      screen: StackWrapper('menu', null, 'Profile', { ChatScreen }),
       navigationOptions: {
-        tabBarLabel: "Profile",
+        tabBarLabel: 'Profile',
         tabBarIcon: ({ tintColor }) => (
           <Icon name="account-box" color={tintColor} />
-        )
-      }
-    }
+        ),
+      },
+    },
   },
   {
-    initialRouteName: "HomeStack",
-    activeColor: "#DEF2F1",
-    inactiveColor: "#2B7A78",
-    barStyle: { backgroundColor: "#4AA9A8" }
-  }
+    initialRouteName: 'HomeStack',
+    activeColor: '#DEF2F1',
+    inactiveColor: '#2B7A78',
+    barStyle: { backgroundColor: '#4AA9A8' },
+  },
 );
 
 export default TabNavigator;
