dc1999524938a719761f248dbcb0548fa20ac94a
Kryštof Woldřich <krystof51@gmail.com>
2018-09-29 16:47:09 +0300

    message screen UI

--- client/containers/ChatScreen.js
+++ client/containers/ChatScreen.js
@@ -1,64 +1,80 @@
-import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
-import { Icon } from 'react-native-elements'
-import Text from '../components/MyText'
+import React, { Component } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
+import { Icon } from "react-native-elements";
+import Text from "../components/MyText";
 
 class ChatScreen extends Component {
   static navigationOptions = {
-    drawerLabel: 'Conversation',
-    drawerIcon: ({ tintColor }) => <Icon name="chat" color={tintColor} />,
+    drawerLabel: "Conversation",
+    drawerIcon: ({ tintColor }) => <Icon name="chat" color={tintColor} />
   };
 
-  state = {
-    refreshing: false,
-    chats: [
-      {
-        name: 'Martynas Gudaitis',
-        message: "Are you planing to go to the party tomorrow?",
-        key: '90',
-      }
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    };
+  }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ messages: responseJson });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.chatContainer}>
+    const chatRender = ({ item }) => {
+      const chat = item;
+
+      return (
+        <View style={styles.conversation}>
           <View style={styles.col1}>
             <Icon name="person" color="grey" />
           </View>
           <View style={styles.col2}>
-            <Text>User ghkjghkjhk</Text>
-            <Text>User message</Text>
+            <Text>{chat.name}</Text>
+            <Text>{chat.email}</Text>
           </View>
         </View>
+      );
+    };
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.messages}
+          renderItem={chatRender}
+          keyExtractor={(item, index) => JSON.stringify(item.id)}
+        />
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  chatContainer: {
-    paddingVertical: 23,
-    flexDirection: 'row',
+  conversation: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 16,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
+    borderBottomColor: "#ccc"
+  },
+  container: {
+    backgroundColor: "white"
   },
   col1: {
     flex: 0.2,
-    paddingHorizontal: 10,
-    alignItems: 'center'
+    paddingHorizontal: 10
   },
   col2: {
     flex: 0.8,
     paddingHorizontal: 10
   }
-})
+});
 
-export default ChatScreen
+export default ChatScreen;
