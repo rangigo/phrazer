ea62b5000c7b1151231e8041990bf733599bc3ec
Kryštof Woldřich <krystof51@gmail.com>
2018-10-02 00:04:17 +0300

    Martin's data for user testing

--- client/reducers/feedReducer.js
+++ client/reducers/feedReducer.js
@@ -1,66 +1,29 @@
 import * as actionTypes from "../actions/actionTypes";
 
-const newFeed = [
-  {
-    key: "6",
-    library: "finnish",
-    category: "shopping",
-    phraze: "How much it costs?",
-    translated: "Paljonko se maksaa?",
-    public: false,
-    phrazed: true,
-    favorite: false
-  },
-  {
-    key: "7",
-    library: "finnish",
-    category: "shopping",
-    phraze: "Where can I found farmer market?",
-    translated: "Missä päin tori on?",
-    public: false,
-    phrazed: true,
-    favorite: false
-  },
-  {
-    key: "8",
-    library: "finnish",
-    category: "shopping",
-    phraze: "One liter of blueberries, please",
-    translated: "Litra mustikoita, kiitos",
-    public: false,
-    phrazed: true,
-    favorite: false
-  },
-  {
-    key: "10",
-    library: "finnish",
-    category: "shopping",
-    phraze: "Where can I find plates?",
-    translated: "Missä lautaset ovat?",
-    public: false,
-    phrazed: true,
-    favorite: false
-  }
-];
-
 const feedInit = [
   {
-    author: { fullName: "Martynas Gala" },
-    native: "How are you?",
-    foreign: "Jak se máš?",
-    key: "1"
+    author: { fullName: "Martynas Gudaitis" },
+    native: "How much it costs?",
+    foreign: "Paljonko se maksaa?",
+    key: "6"
   },
   {
     author: { fullName: "Lena Schwarz" },
-    native: "What's your name?",
+    native: "Where can I found farmers market?",
     foreign: null,
-    key: "2"
+    key: "7"
   },
   {
     author: { fullName: "Krystof Woldrich" },
-    native: "Where do you come from?",
-    foreign: null,
-    key: "3"
+    native: "One liter of blueberries, please.",
+    foreign: "Litra mustikoita, kiitos",
+    key: "8"
+  },
+  {
+    author: { fullName: "Paulius Karlonas" },
+    native: "Where can I find plates?",
+    foreign: "Missä lautaset ovat?",
+    key: "10"
   }
 ];
 
